e7238ce76a88e58edc776e5861197dcbf7f766d9
Kevin Salchert <kevin.salchert@me.com>
2014-11-07 16:28:12 -0800

    Commit of Unity project

---
+++ Assets/Scripts/BoardSetup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardSetup : MonoBehaviour
+{
+	[SerializeField] GameObject whitePrefab;
+	[SerializeField] GameObject redPrefab;
+	[SerializeField] Transform[] whiteStartPositions;
+	[SerializeField] Transform[] redStartPositions;
+	int whiteSpawnCount = 1;
+	int redSpawnCount = 1;
+
+	void Start ()
+	{
+		foreach (Transform whiteStartPosition in whiteStartPositions)
+		{
+			var clone = Instantiate(whitePrefab, whiteStartPosition.position, whiteStartPosition.rotation);
+			clone.name = "White_" + whiteSpawnCount;
+			whiteSpawnCount++;
+		}
+
+		foreach (Transform redStartPosition in redStartPositions)
+		{
+			var clone = Instantiate(redPrefab, redStartPosition.position, redStartPosition.rotation);
+			clone.name = "Red_" + redSpawnCount;
+			redSpawnCount++;
+		}
+	}
+}
