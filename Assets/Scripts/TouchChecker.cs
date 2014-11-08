c2ce62d0bfdb835d87a8ae62f01a603c23cc86ea
Kevin Salchert <kevin.salchert@me.com>
2014-11-07 16:54:40 -0800

    Checkers can now be selected

---
+++ Assets/Scripts/TouchChecker.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchChecker : MonoBehaviour
+{	
+	void OnEnable()
+	{
+		EasyTouch.On_TouchStart += On_TouchStart;
+	}
+	
+	void OnDisable()
+	{
+		UnsubscribeEvent();
+	}
+	
+	void OnDestroy()
+	{
+		UnsubscribeEvent();
+	}
+	
+	void UnsubscribeEvent()
+	{
+		EasyTouch.On_TouchStart -= On_TouchStart;
+	}
+
+	public void On_TouchStart(Gesture gesture)
+	{
+		if (gesture.pickObject == gameObject)
+			Debug.Log("Selected " + gameObject.name);
+	}
+}
